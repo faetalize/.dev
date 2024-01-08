e192640b2052cbe7744dcf0eb35e407412ba4125
fae <faetalize@pm.me>
2024-01-08 21:06:11 +0000

    added audio added credits overlay

--- out/script.js
+++ out/script.js
@@ -1,12 +1,24 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const landingText = document.querySelector('.landingText');
 const pages = document.querySelectorAll('.page');
 const navigationMenu = document.querySelector('.navigationMenu');
 const navigationItems = document.querySelectorAll('.navigationItem');
 const headerBar = document.querySelector('.headerBar');
-const backButton = document.querySelector('.backButton');
+const backButton = document.querySelector('#btn-navigation-back');
 const backgroundImg = document.querySelector('.backgroundImg img');
 const footerBar = document.querySelector('.footerBar');
+const overlay = document.querySelector('.overlay');
+const overlayCloseButton = document.querySelector('#btn-overlay-close');
+const creditsButton = document.querySelector('#credits');
 //keep track of the current page
 let currentPage = null;
 /**
@@ -23,19 +35,52 @@ function hide(element) {
 /**
  * Show an element (with a 0.5s fade in animation)
  * @param {HTMLElement} element The element to show
+ * @param {boolean} transitioning Whether or not the element is transitioning from another page
  **/
-function show(element) {
-    // Wait for other transitions to complete (0.5s delay)
-    setTimeout(function () {
-        // Change display property
+function show(element, transitioning) {
+    if (transitioning) {
+        setTimeout(function () {
+            // Change display property
+            element.style.display = 'flex';
+            // Wait for next frame for display change to take effect
+            requestAnimationFrame(function () {
+                // Start opacity transition
+                element.style.transition = 'opacity 0.5s';
+                element.style.opacity = '1';
+            });
+        }, 500);
+    }
+    else {
         element.style.display = 'flex';
-        // Wait for next frame for display change to take effect
         requestAnimationFrame(function () {
             // Start opacity transition
             element.style.transition = 'opacity 0.5s';
             element.style.opacity = '1';
         });
-    }, 500);
+    }
+}
+/**
+ * Plays audio file with a gradual increase in volume
+ * @param {string} path The path to the audio file
+ * @param {number} initialVolume Initial volume
+ * @param {number} finalVolume Final volume
+ * @param {number} duration_ms Duration of the volume increase
+ * @param {boolean} loop Whether or not to loop the audio
+ */
+function playAudio(path, initialVolume, finalVolume, duration_ms, loop) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let audioElement = new Audio(path);
+        audioElement.volume = initialVolume;
+        audioElement.loop = loop;
+        audioElement.play();
+        let volumeStep = (finalVolume - initialVolume) / duration_ms;
+        let interval = setInterval(() => {
+            audioElement.volume += volumeStep;
+            if (audioElement.volume >= finalVolume) {
+                clearInterval(interval);
+            }
+        }, 1);
+    });
 }
 //hide all elements aside from the landing text
 for (let page of pages)
@@ -43,27 +88,35 @@ for (let page of pages)
 hide(navigationMenu);
 hide(headerBar);
 hide(footerBar);
+hide(overlay);
 //hide the landingtext on click and show the menu
 landingText === null || landingText === void 0 ? void 0 : landingText.addEventListener('click', function () {
+    playAudio('./media/mist.mp3', 0, 0.65, 3000, true);
     hide(landingText);
     if (backgroundImg)
         backgroundImg.style.filter = 'blur(0px)';
-    show(navigationMenu);
-    show(footerBar);
+    show(navigationMenu, true);
+    show(footerBar, true);
+});
+creditsButton === null || creditsButton === void 0 ? void 0 : creditsButton.addEventListener('click', function () {
+    show(overlay, false);
+});
+overlayCloseButton === null || overlayCloseButton === void 0 ? void 0 : overlayCloseButton.addEventListener('click', function () {
+    hide(overlay);
 });
 //navigation
 navigationMenu === null || navigationMenu === void 0 ? void 0 : navigationMenu.addEventListener('click', function (event) {
     if (event.target.classList.contains('navigationItem')) {
         let index = Array.from(navigationItems).indexOf(event.target);
         hide(navigationMenu);
-        show(headerBar);
-        show(pages[index]);
+        show(headerBar, true);
+        show(pages[index], true);
         currentPage = pages[index];
     }
 });
 backButton === null || backButton === void 0 ? void 0 : backButton.addEventListener('click', function () {
     hide(headerBar);
-    show(navigationMenu);
+    show(navigationMenu, true);
     hide(currentPage);
     currentPage = null;
 });
--- script.ts
+++ script.ts
@@ -1,23 +1,26 @@
-const landingText : HTMLElement | null = document.querySelector('.landingText');
-const pages : NodeListOf<HTMLElement> = document.querySelectorAll('.page');
-const navigationMenu : HTMLElement | null = document.querySelector('.navigationMenu');
-const navigationItems : NodeListOf<HTMLElement> = document.querySelectorAll('.navigationItem');
-const headerBar : HTMLElement | null = document.querySelector('.headerBar');
-const backButton : HTMLElement | null = document.querySelector('.backButton');
-const backgroundImg : HTMLElement | null = document.querySelector('.backgroundImg img');
-const footerBar : HTMLElement | null = document.querySelector('.footerBar');
+const landingText: HTMLElement | null = document.querySelector('.landingText');
+const pages: NodeListOf<HTMLElement> = document.querySelectorAll('.page');
+const navigationMenu: HTMLElement | null = document.querySelector('.navigationMenu');
+const navigationItems: NodeListOf<HTMLElement> = document.querySelectorAll('.navigationItem');
+const headerBar: HTMLElement | null = document.querySelector('.headerBar');
+const backButton: HTMLElement | null = document.querySelector('#btn-navigation-back');
+const backgroundImg: HTMLElement | null = document.querySelector('.backgroundImg img');
+const footerBar: HTMLElement | null = document.querySelector('.footerBar');
+const overlay: HTMLElement | null = document.querySelector('.overlay');
+const overlayCloseButton: HTMLElement | null = document.querySelector('#btn-overlay-close');
+const creditsButton: HTMLElement | null = document.querySelector('#credits');
 
 //keep track of the current page
-let currentPage : HTMLElement | null = null;
+let currentPage: HTMLElement | null = null;
 
 /**
 * Hide an element (with a 0.5s fade out animation)
 * @param {HTMLElement} element The element to hide
 **/
-function hide(element : HTMLElement) : void  {
+function hide(element: HTMLElement): void {
     element.style.transition = 'opacity 0.5s';
     element.style.opacity = '0';
-    setTimeout(function() {
+    setTimeout(function () {
         element.style.display = 'none';
     }, 500);
 }
@@ -25,52 +28,94 @@ function hide(element : HTMLElement) : void  {
 /**
  * Show an element (with a 0.5s fade in animation)
  * @param {HTMLElement} element The element to show
+ * @param {boolean} transitioning Whether or not the element is transitioning from another page
  **/
-function show(element : HTMLElement) : void {
-    // Wait for other transitions to complete (0.5s delay)
-    setTimeout(function() {
-        // Change display property
+function show(element: HTMLElement, transitioning: boolean): void {
+    if (transitioning) {
+        setTimeout(function () {
+            // Change display property
+            element.style.display = 'flex';
+            // Wait for next frame for display change to take effect
+            requestAnimationFrame(function () {
+                // Start opacity transition
+                element.style.transition = 'opacity 0.5s';
+                element.style.opacity = '1';
+            });
+        }, 500);
+    }
+    else {
         element.style.display = 'flex';
-        // Wait for next frame for display change to take effect
-        requestAnimationFrame(function() {
+        requestAnimationFrame(function () {
             // Start opacity transition
             element.style.transition = 'opacity 0.5s';
             element.style.opacity = '1';
         });
-    }, 500);
+    }
+}
+
+/**
+ * Plays audio file with a gradual increase in volume
+ * @param {string} path The path to the audio file
+ * @param {number} initialVolume Initial volume
+ * @param {number} finalVolume Final volume
+ * @param {number} duration_ms Duration of the volume increase
+ * @param {boolean} loop Whether or not to loop the audio
+ */
+async function playAudio(path: string, initialVolume: number, finalVolume: number, duration_ms: number, loop: boolean): Promise<void> {
+    let audioElement = new Audio(path);
+    audioElement.volume = initialVolume;
+    audioElement.loop = loop;
+    audioElement.play();
+    let volumeStep = (finalVolume - initialVolume) / duration_ms;
+    let interval = setInterval(() => {
+        audioElement.volume += volumeStep;
+        if (audioElement.volume >= finalVolume) {
+            clearInterval(interval);
+        }
+    }, 1);
 }
 
 //hide all elements aside from the landing text
-for(let page of pages) 
+for (let page of pages)
     hide(page as HTMLElement);
 hide(navigationMenu as HTMLElement);
 hide(headerBar as HTMLElement);
 hide(footerBar as HTMLElement);
+hide(overlay as HTMLElement);
 
 
 //hide the landingtext on click and show the menu
-landingText?.addEventListener('click', function() {
+landingText?.addEventListener('click', function () {
+    playAudio('./media/mist.mp3', 0, 0.65, 3000, true);
     hide(landingText as HTMLElement);
-    if(backgroundImg) 
+    if (backgroundImg)
         backgroundImg.style.filter = 'blur(0px)';
-    show(navigationMenu as HTMLElement);
-    show(footerBar as HTMLElement);
+    show(navigationMenu as HTMLElement, true);
+    show(footerBar as HTMLElement, true);
 });
 
+creditsButton?.addEventListener('click', function () {
+    show(overlay as HTMLElement, false);
+});
+overlayCloseButton?.addEventListener('click', function () {
+    hide(overlay as HTMLElement);
+});
 //navigation
-navigationMenu?.addEventListener('click', function(event) {
+navigationMenu?.addEventListener('click', function (event) {
     if ((event.target as Element).classList.contains('navigationItem')) {
         let index: number = Array.from(navigationItems).indexOf(event.target as HTMLElement);
         hide(navigationMenu as HTMLElement);
-        show(headerBar as HTMLElement);
-        show(pages[index] as HTMLElement);
+        show(headerBar as HTMLElement, true);
+        show(pages[index] as HTMLElement, true);
         currentPage = pages[index];
     }
 });
 
-backButton?.addEventListener('click', function() {
+backButton?.addEventListener('click', function () {
     hide(headerBar as HTMLElement);
-    show(navigationMenu as HTMLElement);
+    show(navigationMenu as HTMLElement, true);
     hide(currentPage as HTMLElement);
     currentPage = null;
 });
+
+
