5f3b362733d46eb669c75288038b68bbed57209a
Valentine Altair <faetalize@pm.me>
2024-07-15 20:17:16 +0100

    sound effects

--- out/script.js
+++ out/script.js
@@ -1,26 +1,27 @@
 "use strict";
-const landingText = document.querySelector('.landingText');
-const pages = document.querySelectorAll('.page');
-const navigationMenu = document.querySelector('.navigationMenu');
-const navigationItems = document.querySelectorAll('.navigationItem');
-const headerBar = document.querySelector('.headerBar');
-const backButton = document.querySelector('#btn-navigation-back');
-const backgroundImg = document.querySelector('.backgroundImg img');
-const footerBar = document.querySelector('.footerBar');
-const overlay = document.querySelector('.overlay');
-const overlayCloseButton = document.querySelector('#btn-overlay-close');
-const creditsButton = document.querySelector('#credits');
+const landingText = document.querySelector(".landingText");
+const pages = document.querySelectorAll(".page");
+const navigationMenu = document.querySelector(".navigationMenu");
+const navigationItems = document.querySelectorAll(".navigationItem");
+const headerBar = document.querySelector(".headerBar");
+const backButton = document.querySelector("#btn-navigation-back");
+const backgroundImg = document.querySelector(".backgroundImg img");
+const footerBar = document.querySelector(".footerBar");
+const overlay = document.querySelector(".overlay");
+const overlayCloseButton = document.querySelector("#btn-overlay-close");
+const creditsButton = document.querySelector("#credits");
+const highlightButtons = document.querySelectorAll(".hbutton");
 //keep track of the current page
 let currentPage = null;
 /**
-* Hide an element (with a 0.5s fade out animation)
-* @param {HTMLElement} element The element to hide
-**/
+ * Hide an element (with a 0.5s fade out animation)
+ * @param {HTMLElement} element The element to hide
+ **/
 function hide(element) {
-    element.style.transition = 'opacity 0.5s';
-    element.style.opacity = '0';
+    element.style.transition = "opacity 0.5s";
+    element.style.opacity = "0";
     setTimeout(function () {
-        element.style.display = 'none';
+        element.style.display = "none";
     }, 500);
 }
 /**
@@ -31,22 +32,22 @@ function hide(element) {
 function show(element, transitioning) {
     if (transitioning) {
         setTimeout(function () {
-            element.style.display = 'flex';
+            element.style.display = "flex";
             // hackery to ensure the transition works on mobile, idk why this works but who fucking cares
             requestAnimationFrame(function () {
                 requestAnimationFrame(function () {
-                    element.style.transition = 'opacity 0.5s';
-                    element.style.opacity = '1';
+                    element.style.transition = "opacity 0.5s";
+                    element.style.opacity = "1";
                 });
             });
         }, 500);
     }
     else {
-        element.style.display = 'flex';
+        element.style.display = "flex";
         requestAnimationFrame(function () {
             requestAnimationFrame(function () {
-                element.style.transition = 'opacity 0.5s';
-                element.style.opacity = '1';
+                element.style.transition = "opacity 0.5s";
+                element.style.opacity = "1";
             });
         });
     }
@@ -63,8 +64,13 @@ function playAudio(path, initialVolume, finalVolume, duration_ms, loop) {
     let audioElement = new Audio(path);
     audioElement.volume = initialVolume;
     audioElement.loop = loop;
+    if (initialVolume == finalVolume) {
+        audioElement.play();
+        console.log("no transition, playing audio");
+        return;
+    }
     //start when enough of the audio has loaded
-    audioElement.addEventListener('canplay', function () {
+    audioElement.addEventListener("canplay", function () {
         try {
             audioElement.play();
             let intervalDuration = 100;
@@ -85,24 +91,25 @@ function playAudio(path, initialVolume, finalVolume, duration_ms, loop) {
         }
     });
 }
-//hide the landingtext on click and show the menu
-landingText === null || landingText === void 0 ? void 0 : landingText.addEventListener('click', function () {
-    playAudio('./media/mist.mp3', 0, 0.2, 10000, true);
+//hide the landingtext on click, show the menu, and play the audio
+landingText === null || landingText === void 0 ? void 0 : landingText.addEventListener("click", function () {
+    playAudio("./media/start.mp3", 0.3, 0.8, 1500, false);
+    playAudio("./media/mist.mp3", 0, 0.8, 10000, true);
     hide(landingText);
     if (backgroundImg)
-        backgroundImg.style.filter = 'blur(0px)';
+        backgroundImg.style.filter = "blur(0px)";
     show(navigationMenu, true);
     show(footerBar, true);
 });
-creditsButton === null || creditsButton === void 0 ? void 0 : creditsButton.addEventListener('click', function () {
+creditsButton === null || creditsButton === void 0 ? void 0 : creditsButton.addEventListener("click", function () {
     show(overlay, false);
 });
-overlayCloseButton === null || overlayCloseButton === void 0 ? void 0 : overlayCloseButton.addEventListener('click', function () {
+overlayCloseButton === null || overlayCloseButton === void 0 ? void 0 : overlayCloseButton.addEventListener("click", function () {
     hide(overlay);
 });
 //navigation
-navigationMenu === null || navigationMenu === void 0 ? void 0 : navigationMenu.addEventListener('click', function (event) {
-    if (event.target.classList.contains('navigationItem')) {
+navigationMenu === null || navigationMenu === void 0 ? void 0 : navigationMenu.addEventListener("click", function (event) {
+    if (event.target.classList.contains("navigationItem")) {
         let index = Array.from(navigationItems).indexOf(event.target);
         hide(navigationMenu);
         show(headerBar, true);
@@ -110,7 +117,21 @@ navigationMenu === null || navigationMenu === void 0 ? void 0 : navigationMenu.a
         currentPage = pages[index];
     }
 });
-backButton === null || backButton === void 0 ? void 0 : backButton.addEventListener('click', function () {
+//sound effects
+for (let button of highlightButtons) {
+    button.addEventListener("mouseover", () => {
+        playAudio("./media/hover.mp3", 0.8, 0.8, 10, false);
+    });
+    button.addEventListener("click", () => {
+        if (button.id == "btn-navigation-back") {
+            playAudio("./media/back.mp3", 0.8, 0.8, 0, false);
+        }
+        else {
+            playAudio("./media/select.mp3", 0.8, 0.8, 0, false);
+        }
+    });
+}
+backButton === null || backButton === void 0 ? void 0 : backButton.addEventListener("click", function () {
     hide(headerBar);
     hide(currentPage);
     show(navigationMenu, true);
--- script.ts
+++ script.ts
@@ -1,28 +1,36 @@
-const landingText: HTMLElement | null = document.querySelector('.landingText');
-const pages: NodeListOf<HTMLElement> = document.querySelectorAll('.page');
-const navigationMenu: HTMLElement | null = document.querySelector('.navigationMenu');
-const navigationItems: NodeListOf<HTMLElement> = document.querySelectorAll('.navigationItem');
-const headerBar: HTMLElement | null = document.querySelector('.headerBar');
-const backButton: HTMLElement | null = document.querySelector('#btn-navigation-back');
-const backgroundImg: HTMLElement | null = document.querySelector('.backgroundImg img');
-const footerBar: HTMLElement | null = document.querySelector('.footerBar');
-const overlay: HTMLElement | null = document.querySelector('.overlay');
-const overlayCloseButton: HTMLElement | null = document.querySelector('#btn-overlay-close');
-const creditsButton: HTMLElement | null = document.querySelector('#credits');
+const landingText: HTMLElement | null = document.querySelector(".landingText");
+const pages: NodeListOf<HTMLElement> = document.querySelectorAll(".page");
+const navigationMenu: HTMLElement | null =
+  document.querySelector(".navigationMenu");
+const navigationItems: NodeListOf<HTMLElement> =
+  document.querySelectorAll(".navigationItem");
+const headerBar: HTMLElement | null = document.querySelector(".headerBar");
+const backButton: HTMLElement | null = document.querySelector(
+  "#btn-navigation-back"
+);
+const backgroundImg: HTMLElement | null =
+  document.querySelector(".backgroundImg img");
+const footerBar: HTMLElement | null = document.querySelector(".footerBar");
+const overlay: HTMLElement | null = document.querySelector(".overlay");
+const overlayCloseButton: HTMLElement | null =
+  document.querySelector("#btn-overlay-close");
+const creditsButton: HTMLElement | null = document.querySelector("#credits");
+const highlightButtons: NodeListOf<HTMLElement> =
+  document.querySelectorAll(".hbutton");
 
 //keep track of the current page
 let currentPage: HTMLElement | null = null;
 
 /**
-* Hide an element (with a 0.5s fade out animation)
-* @param {HTMLElement} element The element to hide
-**/
+ * Hide an element (with a 0.5s fade out animation)
+ * @param {HTMLElement} element The element to hide
+ **/
 function hide(element: HTMLElement): void {
-    element.style.transition = 'opacity 0.5s';
-    element.style.opacity = '0';
-    setTimeout(function () {
-        element.style.display = 'none';
-    }, 500);
+  element.style.transition = "opacity 0.5s";
+  element.style.opacity = "0";
+  setTimeout(function () {
+    element.style.display = "none";
+  }, 500);
 }
 
 /**
@@ -31,27 +39,26 @@ function hide(element: HTMLElement): void {
  * @param {boolean} transitioning Whether or not the element is transitioning from another page
  **/
 function show(element: HTMLElement, transitioning: boolean): void {
-    if (transitioning) {
-        setTimeout(function () {
-            element.style.display = 'flex';
-            // hackery to ensure the transition works on mobile, idk why this works but who fucking cares
-            requestAnimationFrame(function () {
-                requestAnimationFrame(function () {
-                    element.style.transition = 'opacity 0.5s';
-                    element.style.opacity = '1';
-                });
-            });
-        }, 500);
-    }
-    else {
-        element.style.display = 'flex';
+  if (transitioning) {
+    setTimeout(function () {
+      element.style.display = "flex";
+      // hackery to ensure the transition works on mobile, idk why this works but who fucking cares
+      requestAnimationFrame(function () {
         requestAnimationFrame(function () {
-            requestAnimationFrame(function () {
-                element.style.transition = 'opacity 0.5s';
-                element.style.opacity = '1';
-            });
+          element.style.transition = "opacity 0.5s";
+          element.style.opacity = "1";
         });
-    }
+      });
+    }, 500);
+  } else {
+    element.style.display = "flex";
+    requestAnimationFrame(function () {
+      requestAnimationFrame(function () {
+        element.style.transition = "opacity 0.5s";
+        element.style.opacity = "1";
+      });
+    });
+  }
 }
 
 /**
@@ -62,64 +69,89 @@ function show(element: HTMLElement, transitioning: boolean): void {
  * @param {number} duration_ms Duration of the volume increase
  * @param {boolean} loop Whether or not to loop the audio
  */
-function playAudio(path: string, initialVolume: number, finalVolume: number, duration_ms: number, loop: boolean): void {
-    let audioElement = new Audio(path);
-    audioElement.volume = initialVolume;
-    audioElement.loop = loop;
-    //start when enough of the audio has loaded
-    audioElement.addEventListener('canplay', function () {
-        try {
-            audioElement.play();
-            let intervalDuration = 100;
-            let steps = duration_ms / intervalDuration;
-            let volumeStep = (finalVolume - initialVolume) / steps;
-            let interval = setInterval(() => {
-                if (audioElement.volume + volumeStep < finalVolume) {
-                    audioElement.volume += volumeStep;
-                } else {
-                    audioElement.volume = finalVolume;
-                    clearInterval(interval);
-                }
-            }, intervalDuration);
+function playAudio(
+  path: string,
+  initialVolume: number,
+  finalVolume: number,
+  duration_ms: number,
+  loop: boolean
+): void {
+  let audioElement = new Audio(path);
+  audioElement.volume = initialVolume;
+  audioElement.loop = loop;
+  if (initialVolume == finalVolume) {
+    audioElement.play();
+    console.log("no transition, playing audio");
+    return;
+  }
+  //start when enough of the audio has loaded
+  audioElement.addEventListener("canplay", function () {
+    try {
+      audioElement.play();
+      let intervalDuration = 100;
+      let steps = duration_ms / intervalDuration;
+      let volumeStep = (finalVolume - initialVolume) / steps;
+      let interval = setInterval(() => {
+        if (audioElement.volume + volumeStep < finalVolume) {
+          audioElement.volume += volumeStep;
+        } else {
+          audioElement.volume = finalVolume;
+          clearInterval(interval);
         }
-        catch (error) {
-            console.log(error);
-        }
-    });
+      }, intervalDuration);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 
-
-//hide the landingtext on click and show the menu
-landingText?.addEventListener('click', function () {
-    playAudio('./media/mist.mp3', 0, 0.2, 10000, true);
-    hide(landingText as HTMLElement);
-    if (backgroundImg)
-        backgroundImg.style.filter = 'blur(0px)';
-    show(navigationMenu as HTMLElement, true);
-    show(footerBar as HTMLElement, true);
+//hide the landingtext on click, show the menu, and play the audio
+landingText?.addEventListener("click", function () {
+  playAudio("./media/start.mp3", 0.3, 0.8, 1500, false);
+  playAudio("./media/mist.mp3", 0, 0.8, 10000, true);
+  hide(landingText as HTMLElement);
+  if (backgroundImg) backgroundImg.style.filter = "blur(0px)";
+  show(navigationMenu as HTMLElement, true);
+  show(footerBar as HTMLElement, true);
 });
 
-creditsButton?.addEventListener('click', function () {
-    show(overlay as HTMLElement, false);
+creditsButton?.addEventListener("click", function () {
+  show(overlay as HTMLElement, false);
 });
 
-overlayCloseButton?.addEventListener('click', function () {
-    hide(overlay as HTMLElement);
+overlayCloseButton?.addEventListener("click", function () {
+  hide(overlay as HTMLElement);
 });
 //navigation
-navigationMenu?.addEventListener('click', function (event) {
-    if ((event.target as Element).classList.contains('navigationItem')) {
-        let index: number = Array.from(navigationItems).indexOf(event.target as HTMLElement);
-        hide(navigationMenu as HTMLElement);
-        show(headerBar as HTMLElement, true);
-        show(pages[index] as HTMLElement, true);
-        currentPage = pages[index];
-    }
+navigationMenu?.addEventListener("click", function (event) {
+  if ((event.target as Element).classList.contains("navigationItem")) {
+    let index: number = Array.from(navigationItems).indexOf(
+      event.target as HTMLElement
+    );
+    hide(navigationMenu as HTMLElement);
+    show(headerBar as HTMLElement, true);
+    show(pages[index] as HTMLElement, true);
+    currentPage = pages[index];
+  }
 });
 
-backButton?.addEventListener('click', function () {
-    hide(headerBar as HTMLElement);
-    hide(currentPage as HTMLElement);
-    show(navigationMenu as HTMLElement, true);
-    currentPage = null;
+//sound effects
+for (let button of highlightButtons) {
+  button.addEventListener("mouseover", () => {
+    playAudio("./media/hover.mp3", 0.8, 0.8, 10, false);
+  });
+  button.addEventListener("click", () => {
+    if (button.id == "btn-navigation-back") {
+      playAudio("./media/back.mp3", 0.8, 0.8, 0, false);
+    } else {
+      playAudio("./media/select.mp3", 0.8, 0.8, 0, false);
+    }
+  });
+}
+
+backButton?.addEventListener("click", function () {
+  hide(headerBar as HTMLElement);
+  hide(currentPage as HTMLElement);
+  show(navigationMenu as HTMLElement, true);
+  currentPage = null;
 });
